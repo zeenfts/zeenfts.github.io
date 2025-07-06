36bf67e6b395a105d2d840f851343cd1bfa5614c
imdifagama <imdifagama@gmail.com>
2025-07-06 17:37:19 +0800

    update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,6 @@
   import '../app.css';
   import '../lib/i18n';
   import { waitLocale } from 'svelte-i18n';
-
-  export const prerender = true;
-  export const trailingSlash = 'always';
 </script>
 
 {#await waitLocale()}
